e827d6ddf0f2ea1230484b8de47425294bbb728d
victorsouza2382 <victorsouza2382@gmail.com>
2022-06-21 22:08:52 -0300

    Novo Commit

--- index.js
+++ index.js
@@ -5,34 +5,34 @@
   const vendedor = require('./import_cars/models/vendedorModel');
   await db.sync({ force: true });
 
-  const novovendedor = await vendedor.create({ nome: 'Mauricio', sexo: '0' });
+  /*const novovendedor = await vendedor.create({ nome: 'Mauricio', sexo: '0' });*/
   const carro = await CanvasRenderingContext2D.findByPk(1, {
     include: vendedor
   });
   //await carro.setvendedor([novovendedor]);
-  console.log(carro.categoria);
+  // console.log(carro.vendedor);
 
-  const novoCliente = await cliente.create({
+  /*const novoCliente = await cliente.create({
     nome: 'Dev'
-  })
+  })*/
 
-  const novoVeiculo = await veiculo.create({
-    nome: 'Gol',
-    modelo: 'Trend Line',
-    marca: 'volkswagen',
-    ano_fab: '2022',
-    idcliente: novoCliente.id
-  })
-  console.log(novoProduto);
+  /*const novoVeiculo = await veiculo.create({
+     nome: 'Gol',
+     modelo: 'Trend Line',
+     marca: 'volkswagen',
+     ano_fab: '2022',
+     idcliente: novoCliente.id
+   })
+   console.log(novoProduto);*/
 
 
   /*const veiculo = await veiculo.findByPk(1, {include: Cliente });
   console.log(veiculo.cliente.nome);*/
 
 
-  const cliente = await cliente.findByPk(1);
+  /*const cliente = await cliente.findByPk(1);
   const veiculo = cliente.getVeiculo();
-  console.log(cliente.veiculo);
+  console.log(cliente.veiculo);*/
 
 
   /*await veiculo.destroy();*/
